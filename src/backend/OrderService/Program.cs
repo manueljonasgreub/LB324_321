b8df2c62190de5897f35d60f40c551b3e8ef3cf8
m.greub.inf22 <M.Greub.inf22@stud.bbbaden.ch>
2025-01-11 21:22:18 +0100

    changed program.cs

--- src/backend/OrderService/Program.cs
+++ src/backend/OrderService/Program.cs
@@ -32,7 +32,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<OrderContext>();
-    
+    db.Database.EnsureCreated();
 }
 
 // Configure the HTTP request pipeline.
