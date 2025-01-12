e0118b377a4e20ef69c7c560900e81e7a814b204
m.greub.inf22 <M.Greub.inf22@stud.bbbaden.ch>
2025-01-12 14:57:17 +0100

    Update Program.cs

--- src/backend/ProductService/Program.cs
+++ src/backend/ProductService/Program.cs
@@ -32,7 +32,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ProductContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 // Configure the HTTP request pipeline.
